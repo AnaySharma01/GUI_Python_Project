cb568245f2255954b03867b8c672db2648870901
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2023-11-09 16:40:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -84,9 +84,9 @@ def start():
 def right():
     if 'username' in session:
           # Right turn
-          kit.motor1.throttle = 0.72
-          kit.motor2.throttle = -0.72
-          time.sleep(0.5)
+          kit.motor1.throttle = -0.72
+          kit.motor2.throttle = 0.72
+          time.sleep(0.3)
 
           return jsonify("right")
     else:
@@ -98,7 +98,7 @@ def forward():
           kit.motor1.throttle = 0.732
           kit.motor2.throttle = 0.7
         # Run both motors for 3.5 seconds
-          time.sleep(0.5)
+          time.sleep(0.3)
 
           return jsonify("forward")
     else:
@@ -110,7 +110,7 @@ def backward():
           kit.motor1.throttle = -0.81
           kit.motor2.throttle = -0.7
           #Run both motors for 3.5 seconds
-          time.sleep(0.5)
+          time.sleep(0.3)
           return jsonify("backward")
     else:
        return jsonify("not logged in")
@@ -120,7 +120,7 @@ def left():
          #Left turn
          kit.motor1.throttle = 0.72
          kit.motor2.throttle = -0.75
-         time.sleep(0.5)
+         time.sleep(0.3)
 
          return jsonify("left")
     else:
