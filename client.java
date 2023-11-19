22577e34b59380227c829da67af60ae5ab066304
Emily Huang <144239497+EmilyHuang111@users.noreply.github.com>
2023-11-19 14:09:17 -0500

    Update client.java

--- client.java
+++ client.java
@@ -13,29 +13,45 @@ public class Main extends JFrame {
 
     public Main() {
         setTitle("Robot Controller");
-        setSize(300, 200);
+        setSize(400, 300); // Adjusted the size for the new grid
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Create buttons with arrow symbols
-        JButton forwardButton = new JButton("\u2191"); // Up arrow
-        JButton backwardButton = new JButton("\u2193"); // Down arrow
-        JButton leftButton = new JButton("\u2190"); // Left arrow
-        JButton rightButton = new JButton("\u2192"); // Right arrow
+        // Create buttons with arrow symbols (3 times bigger)
+        JButton forwardButton = createArrowButton("\u2191", 3); // Up arrow
+        JButton backwardButton = createArrowButton("\u2193", 3); // Down arrow
+        JButton leftButton = createArrowButton("\u2190", 3); // Left arrow
+        JButton rightButton = createArrowButton("\u2192", 3); // Right arrow
         JButton stopButton = new JButton("Stop");
+        JButton startButton = new JButton("Start");
+
+        // Set size for Start and Stop buttons to 50%
+        Dimension buttonSize = new Dimension(50, 50);
+        startButton.setPreferredSize(buttonSize);
+        stopButton.setPreferredSize(buttonSize);
+
+        // Set background color for Start button to green and Stop button to red
+        startButton.setBackground(Color.GREEN);
+        stopButton.setBackground(Color.RED);
 
         // Create a panel with a GridLayout
-        JPanel panel = new JPanel(new GridLayout(3, 3));
+        JPanel panel = new JPanel(new GridLayout(3, 5));
 
-        // Add empty labels for layout purposes
-        panel.add(new JLabel());
-        panel.add(forwardButton);
-        panel.add(new JLabel());
-        panel.add(leftButton);
-        panel.add(stopButton);
-        panel.add(rightButton);
-        panel.add(new JLabel());
-        panel.add(backwardButton);
-        panel.add(new JLabel());
+        // Add buttons to the panel at specified positions
+        panel.add(new JLabel());    // (1,1)
+        panel.add(new JLabel());    // (1,2)
+        panel.add(forwardButton);   // (1,3)
+        panel.add(new JLabel());    // (1,4)
+        panel.add(new JLabel());    // (1,5)
+        panel.add(leftButton);      // (2,1)
+        panel.add(startButton);     // (2,2)
+        panel.add(new JLabel());    // (2,3)
+        panel.add(stopButton);      // (2,4) - Moved to the second to the last column
+        panel.add(rightButton);     // (2,5)
+        panel.add(new JLabel());    // (3,1)
+        panel.add(new JLabel());    // (3,2)
+        panel.add(backwardButton);  // (3,3)
+        panel.add(new JLabel());    // (3,4)
+        panel.add(new JLabel());    // (3,5)
 
         // Add action listeners to buttons
         forwardButton.addActionListener(new ButtonClickListener("/forward"));
@@ -43,6 +59,7 @@ public class Main extends JFrame {
         leftButton.addActionListener(new ButtonClickListener("/left"));
         rightButton.addActionListener(new ButtonClickListener("/right"));
         stopButton.addActionListener(new ButtonClickListener("/stop"));
+        startButton.addActionListener(new ButtonClickListener("/start"));
 
         getContentPane().add(panel);
         setVisible(true);
@@ -84,6 +101,14 @@ public class Main extends JFrame {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
+
+    private JButton createArrowButton(String text, int scaleFactor) {
+        JButton button = new JButton(text);
+        Font originalFont = button.getFont();
+        Font newFont = new Font(originalFont.getName(), originalFont.getStyle(), originalFont.getSize() * scaleFactor);
+        button.setFont(newFont);
+        return button;
     }
 
     public static void main(String[] args) {
