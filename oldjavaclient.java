c5d95a28e51acd7b4522181f625c8c18e169c8e3
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2023-11-20 08:41:39 -0500

    Update oldjavaclient.java

--- oldjavaclient.java
+++ oldjavaclient.java
@@ -1,3 +1,13 @@
+//Imports libraries
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.plaf.basic.BasicArrowButton;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class JavaClient extends JFrame {
@@ -46,7 +56,7 @@ public class JavaClient extends JFrame {
         textPanel.add(new JLabel("Time:"));
         textPanel.add(textTime);
         pack();
-        
+
         //Calls movement functions for buttons from client
         forwardBtn.addActionListener(new ActionListener() {
             @Override
