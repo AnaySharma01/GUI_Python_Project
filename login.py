c2ff1436352cffea1c1391e86c6b1786da873149
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2023-12-03 22:12:01 -0500

    Update login.py

--- login.py
+++ login.py
@@ -4,7 +4,7 @@ from flask import request,render_template,redirect,url_for
 import sqlite3 
 import bcrypt
 from GUI import app,get_db_connection
-
+import requests
 #navigates to login page
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -21,4 +21,19 @@ def login():
       else:
          print("User/ Password Error")
 
-    return render_template('login.html')
+    return render_template('login.html') 
+    
+@app.route('/restlogin')
+def restlogin( ):
+    if request.method == 'GET':
+      user=request.args.get('user')
+      password=request.args.get('password')
+      conn = get_db_connection()
+      user = conn.execute('SELECT * from user where username = ? ',
+                          (user,)).fetchone()
+      conn.close()
+      if bcrypt.checkpw(password.encode("utf-8"),str(user["password"]).encode("utf-8")):
+          session['username'] = user["first_name"]
+          return jsonify("success")
+      else:
+           return jsonify("Login Error")
