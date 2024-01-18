00c2a8f377549eee82ae23e66a56b86911df179b
Anay Sharma <115898324+AnaySharma01@users.noreply.github.com>
2024-01-18 08:39:56 -0500

    Update app4.py

--- app4.py
+++ app4.py
@@ -28,91 +28,136 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-#Creates a mask around the frame
-def crop_image(image, vertices):
-    #Defines the mask using numpy
-    mask = np.zeros_like(image)
-    #Fills the mask
-    mask_color = 255
-    cv.fillPoly(mask, vertices, mask_color)
-    masked_image = cv.bitwise_and(image, mask)
-    #Returns the mask when called
-    return masked_image
-#Creates the video overlay
-def display_lines(image, lines):
-    #Creates numpy array for the frame
-    image = np.copy(image)
-    blank_image = np.zeros((image.shape[0], image.shape[1], 3), dtype=np.uint8)
-    try:
-        #Creates lines if the camera detects a lane
-        for line in lines:
-            for x1, y1, x2, y2 in line:
-                cv.line(blank_image, (x1,y1), (x2,y2), (0, 255, 0), thickness=10)
-        image = cv.addWeighted(image, 0.8, blank_image, 1, 0.0)
-        return image
-    #If it does not detect, return the video feed without the lines
-    except: return image
-    #Creates and displays the center line
-    finally: cv.line(image, pt1=(500, 400), pt2=(500, 800), color=(0, 255, 0), thickness=10)
+# Masking
+def mask_img(img):  # H  S  V
+    lower_thr = np.array([0, 0, 0])
+    upper_thr = np.array([179, 255, 87])
+    img_masked = cv2.inRange(img, lower_thr, upper_thr)
+    return img_masked
 
-#Displays the frame overlay
-def process_image(image):
-    #Sets the dimensions of the overlay lines and mask
-    height = image.shape[0]
-    width = image.shape[1]
-    masked_image_vertices = [
-        (0, height),
-        (width/2, height/2),
-        (width, height)
-    ]
-    #Image processes the frame using gaussian blur and canny edge detection
-    gray_image = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
-    gaussian_image = cv.GaussianBlur(gray_image, (7, 7),1)
-    canny_image = cv.Canny(gaussian_image, 100, 120)
-    cropped_image = crop_image(canny_image,
-    np.array([masked_image_vertices], np.int32))
-    #Processes the lines and displays them when called
-    lines = cv.HoughLinesP(cropped_image, rho=3, theta=np.pi/180, threshold=215, lines=np.array([]), minLineLength=75, maxLineGap=200)
-    image_with_lines = display_lines(image, lines)
-    return image_with_lines
+# Canny Edge Detection
+def detect_edges(img):
+  img_gre = img if len(img.shape) == 2 else cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  blur = cv2.GaussianBlur(img_gre, (5, 5), 0)
+  img_canny = cv2.Canny(blur, 200, 400)
+  return img_canny
+
+# Cropping , Region of Interest
+def crop_roi(img):
+    height = img.shape[0]
+    width = img.shape[1]
+    # print(height, width)
+    mask = np.zeros_like(img)
+    cv2.rectangle(mask, (0, height // 2), (width, height), 255, -1)  # -1 -> fill
+    roi = cv2.bitwise_and(img, mask)
+    return roi
+
+# Hough Transform
+def detect_lines(img):
+    # function that we use is HoughLinesP
+    rho = 1
+    theta = np.pi / 180
+    min_threshold = 10
+    min_line_length = 20
+    max_line_gap = 4
+    lines = cv2.HoughLinesP(img, rho, theta, min_threshold, np.array([]), min_line_length, max_line_gap)
+    return lines
+  
+# Group positive slope and negative slope lines, which will define left and right lane markings
+def group_lines(img, lines):
+    height = img.shape[0]
+    width = img.shape[1]
+    lane_lines = []
+    # No lines found
+    if lines is None:
+        return lane_lines
+    left_lane = []
+    right_lane = []
+    boundary = 1 / 3
+    left_lane_area_width = width * (1 - boundary)
+    right_lane_area_width = width * boundary
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            # skip vertical lines as they have infinite slope
+            if x1 == x2:
+                continue
+            coff = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = coff[0]
+            intercept = coff[1]
+            # positive slop -> right lane marking  \
+            #                                       \
+            #                                        \
+            #                                         \
+            if slope > 0:
+                # search area check
+                if x1 > right_lane_area_width and x2 > right_lane_area_width:
+                    right_lane.append((slope, intercept))
+            # negative slop -> left lane marking  /
+            #                                    /
+            #                                   /
+            #                                  /
+            else:
+                if x1 < left_lane_area_width and x2 < left_lane_area_width:
+                    left_lane.append((slope, intercept))
+
+    # averaging all the lines in each group to get a single line out of them
+    left_avg = np.average(left_lane, axis=0)
+    right_avg = np.average(right_lane, axis=0)
+
+    # if got left lane, convert to point form from intercept form
+    if len(left_lane) > 0:
+        lane_lines.append(line_to_point(img, left_avg))
+    if len(right_lane) > 0:
+        lane_lines.append((line_to_point(img, right_avg)))
+    return lane_lines
+
+# Create points from the lane lines with slop and intercept
+def line_to_point(img, line):
+    slop = line[0]
+    intercept = line[1]
+    height = img.shape[0]
+    width = img.shape[1]
+    y1 = int(height / 2)  # middle
+    x1 = int((y1 - intercept) / slop)
+    if x1 < 0:
+        x1 = 0
+    if x1 > width:
+        x1 = width
+    y2 = int(height)  # bottom
+    x2 = int((y2 - intercept) / slop)
+    if x2 < 0:
+        x2 = 0
+    if x2 > width:
+        x2 = width
+    return [[x1, y1, x2, y2]]
 
 def generate_raw_video():
-  
- #Starts the video
- global video
- webcam = cv.VideoCapture(0)
- 
- #Creates the video dimensions
- width, height = 800, 600
- 
- #Sets the video dimensions
- webcam.set(cv.CAP_PROP_FRAME_WIDTH, width)
- webcam.set(cv.CAP_PROP_FRAME_HEIGHT, height)
- 
- # Set the desired frame rate
- frame_rate = 20  # You can adjust this value
- prev_frame_time = 0
+  global webcam
+  webcam = cv2.VideoCapture(0)
+  webcam.set(cv2.CAP_PROP_FRAME_WIDTH, 256)  # Set width
+  webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, 256)  # Set height
 
- while True:
-     time_elapsed = time.time() - prev_frame_time
-     success, frame = video.read()
-     if not success:
-         break
-     if time_elapsed > 1./frame_rate:
-         prev_frame_time = time.time()
-         # Apply JPEG compression
-         ret, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 85])  
-         frame = buffer.tobytes()
-         yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+  # Set the desired frame rate
+  frame_rate = 5  # You can adjust this value
+  prev_frame_time = 0
+
+  while True:
+      time_elapsed = time.time() - prev_frame_time
+      success, frame = webcam.read()
+      # if not success:
+      #     break
+      # if time_elapsed > 1./frame_rate:
+      #     prev_frame_time = time.time()
+      #     # Apply JPEG compression
+      #     ret, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 85])  
+      #     frame = buffer.tobytes()
+      #     yield (b'--frame\r\n'
+      #            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
         
 def generate_processed_video():
     global webcam
     webcam.set(cv2.CAP_PROP_FRAME_WIDTH, 256)  # Set width
     webcam.set(cv2.CAP_PROP_FRAME_HEIGHT, 256)  # Set height
-    # Set the desired frame rate
-    frame_rate = 20  # Adjust as needed
-    prev_frame_time = 0
     # Set the desired frame rate
     frame_rate = 20  # Adjust as needed
     prev_frame_time = 0
@@ -330,4 +375,4 @@ def video_feed_processed():
 
 #Allows app to run
 if __name__ == '__main__':
-    app.run(host='192.168.1.28', port=3333)
+    app.run(host='192.168.1.28', port=4444)
